c48e7221723c0a9ae2aeb93367996122d5f8fcea
Jon Callahan <jon_callahan@intuit.com>
2021-12-08 17:29:16 -0600

    feat: move to remix 💿

---
+++ api/README.md
@@ -0,0 +1,1 @@
+The `api/index.js` file is needed for Vercel to host a Remix project. Don't delete it.
---
+++ app/root.tsx
@@ -0,0 +1,135 @@
+import { Links, LiveReload, Meta, Outlet, Scripts, useCatch } from "remix";
+import type { LinksFunction } from "remix";
+
+import globalStylesUrl from "~/styles/global.css";
+import normalizeStylesUrl from "~/styles/normalize.min.css";
+
+// https://remix.run/api/app#links
+export let links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: normalizeStylesUrl,
+    },
+    { rel: "stylesheet", href: globalStylesUrl },
+  ];
+};
+
+// https://remix.run/api/conventions#default-export
+// https://remix.run/api/conventions#route-filenames
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+// https://remix.run/docs/en/v1/api/conventions#errorboundary
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <Document title="Error!">
+      <Layout>
+        <div>
+          <h1>There was an error</h1>
+          <p>{error.message}</p>
+          <hr />
+          <p>
+            Hey, developer, you should replace this with what you want your
+            users to see.
+          </p>
+        </div>
+      </Layout>
+    </Document>
+  );
+}
+
+// https://remix.run/docs/en/v1/api/conventions#catchboundary
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = (
+        <p>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </p>
+      );
+      break;
+    case 404:
+      message = (
+        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+      );
+      break;
+
+    default:
+      throw new Error(caught.data || caught.statusText);
+  }
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <Layout>
+        <h1>
+          {caught.status}: {caught.statusText}
+        </h1>
+        {message}
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title ? <title>{title}</title> : null}
+        <Meta />
+        <Links />
+        <script
+          data-goatcounter="https://joncallahan.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `if (window.location.hash === '#skipgc') {localStorage.setItem('skipgc', 't')} window.goatcounter = {no_onload: localStorage.getItem('skipgc') === 't' || ('doNotTrack' in navigator && navigator.doNotTrack === '1')}`,
+          }}
+        />
+        <script
+          async
+          src="https://cdn.panelbear.com/analytics.js?site=JdFbYCwwcsZ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
+              panelbear('config', { site: 'JdFbYCwwcsZ' });
+          `,
+          }}
+        />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
